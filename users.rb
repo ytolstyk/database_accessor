148e747140ee10601e9120f39e653f2b3f2620c4
Yuriy Tolstykh <yuriy.tolstykh@gmail.com>
2014-10-07 09:59:01 -0700

    Update users.rb

--- users.rb
+++ users.rb
@@ -48,20 +48,6 @@ class User < DatabaseAccessor
     @database = "users"    
   end
   
-  # moved to super
-  # def save
-#     raise 'already saved!' unless @id.nil?
-#
-#     QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
-#     INSERT INTO
-#       users (fname, lname)
-#     VALUES
-#       (?, ?)
-#     SQL
-#
-#     @id = QuestionsDatabase.instance.last_insert_row_id
-#   end
-  
   def name
     "#{fname} #{lname}"
   end
